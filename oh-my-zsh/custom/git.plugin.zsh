c0005b8036cdf652a8c09d717a8c6bf6b7186615
exAspArk <exaspark@gmail.com>
2016-01-29 11:44:38 +0200

    Add more git aliases

--- oh-my-zsh/custom/git.plugin.zsh
+++ oh-my-zsh/custom/git.plugin.zsh
@@ -26,6 +26,8 @@ alias glr='git pull --rebase'
 alias gp='git push'
 alias gpt='git push --tags'
 
+alias gt='git tag'
+
 alias gm='git merge --no-ff'
 alias gmu='git merge upstream/$(git_current_branch)'
 
@@ -41,7 +43,8 @@ alias gst='git status'
 
 alias gwip='git add -A; git rm $(git ls-files --deleted) 2> /dev/null; git commit -m "--wip--"'
 
-alias glog="git log --graph --abbrev-commit --decorate --date=relative --format=format:'%C(bold blue)%h%C(reset) - %C(bold green)(%ar)%C(reset) %C(white)%s%C(reset) %C(dim white)- %an%C(reset)%C(bold yellow)%d%C(reset)' --all"
+alias glog="git log --graph --abbrev-commit --decorate --date=relative --format=format:'%C(bold yellow)%d%C(reset) %C(white)%s%C(reset) %C(dim white)(%an)%C(reset) (%C(bold blue)%h%C(reset)) (%C(bold green)%ar%C(reset))' --all"
+alias gclog="git log --no-merges --pretty=format:'%s (%an)'"
 
 alias grs='git reset'
 alias grsh='git reset --hard'
