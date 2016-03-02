98fab506383cae286879351b6564372d0be77ed3
exAspArk <exaspark@gmail.com>
2016-03-02 16:01:07 +0200

    Get rid of zshrc plugins

--- zsh/custom/rspec.plugin.zsh
+++ zsh/custom/rspec.plugin.zsh
@@ -1,1 +1,1 @@
-alias spec='function _spec(){ [ -f tmp/rspec_runner  ] && bundle exec rspec_runner "$1" || bundle exec rspec "$1";  }; _spec'
+alias rr='function _rr(){ local pattern=${1:=.} ; [ -f tmp/rspec_runner ] && bundle exec rspec_runner "$pattern" || bundle exec rspec "$pattern"; }; _rr'
