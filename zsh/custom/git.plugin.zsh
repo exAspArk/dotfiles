738fcf47180b09b30fd731287bb2a687c1cd86e8
exAspArk <exaspark@gmail.com>
2016-09-26 15:11:54 +0300

    Add alias to list merged branches

--- zsh/custom/git.plugin.zsh
+++ zsh/custom/git.plugin.zsh
@@ -7,6 +7,7 @@ alias gaa='git add --all'
 alias gb='git branch'
 alias gbd='git branch -d'
 alias gbs='git branch --sort=-committerdate'
+alias gbm='git branch --merged master'
 
 alias gc='git commit -v'
 alias gc!='git commit -v --amend'
